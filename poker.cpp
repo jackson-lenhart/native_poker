b089df68ba04f372e95ceec98d468e870ef61281
unknown <jacksonlenhart@gmail.com>
2021-06-20 12:00:10 -0400

    Fix bug with generating possible_hand combinations on showdown.

--- poker.cpp
+++ poker.cpp
@@ -86,14 +86,14 @@ void sort_combined_hand(card *combined_hand) {
 
 card extract_random_card(card *deck) {
 	int selector = rand() % 52;
-	
+
 	// WARNING: Will result in infinite loop if ALL cards have been removed from deck. Keep in mind during testing.
 	// MAYBE TODO: Use assert here to make sure the deck is not empty.
 	// Also maybe make deck a struct w/ a length property?
 	while (deck[selector].removed_from_deck) {
 		selector = rand() % 52;
 	}
-	
+
 	card c;
 	c = deck[selector];
 	deck[selector].removed_from_deck = true;
@@ -138,16 +138,16 @@ void aces_high_unless_wheel(card *hand) {
 			is_wheel = false;
 		}
 	}
-	
+
 	if (is_wheel) {
 		card output_hand[5];
 		output_hand[0].value = 1;
 		output_hand[0].suit = hand[4].suit;
-		
+
 		for (int i = 1; i < 5; i++) {
 			output_hand[i] = hand[i - 1];
 		}
-		
+
 		for (int i = 0; i < 5; i++) {
 			hand[i] = output_hand[i];
 		}
@@ -159,7 +159,7 @@ bool is_straight(card *hand) {
 	for (int i = 0; i < 4; i++) {
 		if (hand[i + 1].value - hand[i].value != 1) return false;
 	}
-	
+
 	return true;
 }
 
@@ -235,7 +235,7 @@ char* stringify_hand_rank(hand_rank rank) {
 hand_rank evaluate_hand(card *hand) {
 	// sort_hand(hand);
 	aces_high_unless_wheel(hand);
-	
+
 	bool is_flush = true;
 	char suit = hand[0].suit;
 	for (int i = 1; i < 5; i++) {
@@ -243,15 +243,15 @@ hand_rank evaluate_hand(card *hand) {
 			is_flush = false;
 		}
 	}
-	
+
 	bool is_str8 = is_straight(hand);
 	if (is_flush) {
 		if (is_str8) return StraightFlush;
 		else return Flush;
 	}
-	
+
 	if (is_str8) return Straight;
-	
+
 	{
 		// Quads
 		int x = hand[0].value;
@@ -262,7 +262,7 @@ hand_rank evaluate_hand(card *hand) {
 				if (quads_counter == 4) return Quads;
 			}
 		}
-		
+
 		x = hand[1].value;
 		quads_counter = 1;
 		for (int i = 2; i < 5; i++) {
@@ -272,13 +272,13 @@ hand_rank evaluate_hand(card *hand) {
 			}
 		}
 	}
-	
+
 	{
 		// FullHouse
 		int running_index  = 1;
 		int current_value = hand[0].value;
 		int count = 1;
-		
+
 		for (int i = running_index; i < 3; i++) {
 			if (hand[i].value == current_value) {
 				count++;
@@ -287,10 +287,10 @@ hand_rank evaluate_hand(card *hand) {
 				break;
 			}
 		}
-		
+
 		if (count >= 2) {
 			current_value = hand[running_index].value;
-			
+
 			for (int i = running_index; i < 5; i++) {
 				if (hand[i].value == current_value) {
 					if (i == 4) return FullHouse;
@@ -300,30 +300,30 @@ hand_rank evaluate_hand(card *hand) {
 			}
 		}
 	}
-	
+
 	{
 		// ThreeOfAKind
 		for (int i = 0; i < 3; i++) {
 			int current_value = hand[i].value;
 			int count = 1;
-			
+
 			for (int j = i + 1; j < 5; j++) {
 				if (hand[j].value == current_value) count++;
 				else break;
 			}
-			
+
 			if (count == 3) return ThreeOfAKind;
 		}
 	}
-	
+
 	int pairs_count = 0;
 	for (int i = 1; i < 5; i++) {
 		if (hand[i].value == hand[i - 1].value) pairs_count++;
 	}
-	
+
 	if (pairs_count == 1)       return OnePair;
 	else if (pairs_count == 2)  return TwoPair;
-	
+
 	return HighCard;
 }
 
@@ -339,7 +339,7 @@ int resolve_tie(card *hand1, card *hand2, hand_rank rank) {
 			return 0;
 		}
 	}
-	
+
 	if (rank == Flush) {
 		for (int i = 4; i >= 0; i--) {
 			if (hand1[i].value > hand2[i].value) {
@@ -348,10 +348,10 @@ int resolve_tie(card *hand1, card *hand2, hand_rank rank) {
 				return 2;
 			}
 		}
-		
+
 		return 0;
 	}
-	
+
 	if (rank == Quads) {
 		card quad_card1 = hand1[0];
 		card kicker1 = hand1[4];
@@ -359,14 +359,14 @@ int resolve_tie(card *hand1, card *hand2, hand_rank rank) {
 			quad_card1 = hand1[1];
 			kicker1 = hand1[0];
 		}
-		
+
 		card quad_card2 = hand2[0];
 		card kicker2 = hand2[4];
 		if (hand2[1].value != quad_card2.value) {
 			quad_card2 = hand2[1];
 			kicker2 = hand2[0];
 		}
-		
+
 		if (quad_card1.value > quad_card2.value) {
 			return 1;
 		} else if (quad_card1.value < quad_card2.value) {
@@ -382,7 +382,7 @@ int resolve_tie(card *hand1, card *hand2, hand_rank rank) {
 		}
 	}
 }
-	
+
 
 void create_deck(card *deck) {
 	int i = 0;
@@ -405,7 +405,7 @@ bool test_deck_empty(card *deck) {
 			card_count++;
 		}
 	}
-	
+
 	if (card_count < 5) return true;
 	else return false;
 }
@@ -431,7 +431,7 @@ void increment_h_status() {
 
 void get_card_image_id(card c, char *id_buffer) {
 	char stringified_value[3];
-		
+
 	if (c.value < 10) {
 		sprintf(stringified_value, "0%d", c.value);
 	} else if (c.value == 14) {
@@ -439,7 +439,7 @@ void get_card_image_id(card c, char *id_buffer) {
 	} else {
 		sprintf(stringified_value, "%d", c.value);
 	}
-	
+
 	sprintf(id_buffer, "%c%s", c.suit, stringified_value);
 }
 
@@ -449,7 +449,7 @@ int get_card_asset_index(char *id) {
 			return i;
 		}
 	}
-	
+
 	return 0;
 }
 
@@ -462,7 +462,9 @@ struct best_possible_hand_result {
 best_possible_hand_result get_best_possible_hand(card *combined_hand) {
 	const int NUM_POSSIBLE_HANDS = 21;	// Total subsets of 5 possible in 7 cards (hand + board)
 	card possible_hands[5 * NUM_POSSIBLE_HANDS];
-	for (int h = 0; h < NUM_POSSIBLE_HANDS; h++) {
+
+	int h = 0;
+	while (h < 5 * NUM_POSSIBLE_HANDS) {
 		for (int i = 0; i < 3; i++) {
 			for (int j = i + 1; j < 4; j++) {
 				for (int k = j + 1; k < 5; k++) {
@@ -473,13 +475,15 @@ best_possible_hand_result get_best_possible_hand(card *combined_hand) {
 							possible_hands[h + 2] = combined_hand[k];
 							possible_hands[h + 3] = combined_hand[l];
 							possible_hands[h + 4] = combined_hand[m];
+
+							h += 5;
 						}
 					}
 				}
 			}
 		}
 	}
-	
+
 	hand_rank best_hand_rank = evaluate_hand(possible_hands);
 	card *best_hand_ptr = possible_hands;
 	int i = 1;
@@ -490,15 +494,15 @@ best_possible_hand_result get_best_possible_hand(card *combined_hand) {
 			best_hand_rank = curr_hand_rank;
 			best_hand_ptr = curr;
 		}
-		
+
 		curr += 5;
 		i++;
 	}
-	
+
 	best_possible_hand_result result;
 	result.hand_ptr = best_hand_ptr;
 	result.rank = best_hand_rank;
-	
+
 	return result;
 }
 
@@ -507,12 +511,12 @@ void update_and_render(win32_offscreen_buffer *buffer, game_assets *assets, keyb
 		if (k_input->digit_pressed > -1) {
 			char digit_str[2];
 			itoa(k_input->digit_pressed, digit_str, 10);
-			
+
 			int i = 0;
 			while (G_STATE.bet_input[i] != 0 && i < 32) {
 				i++;
 			}
-			
+
 			if (i < 32) {	// Do nothing if we iterated all the way through as that means the buffer is filled with chars.
 				G_STATE.bet_input[i] = digit_str[0];
 			}
@@ -521,13 +525,13 @@ void update_and_render(win32_offscreen_buffer *buffer, game_assets *assets, keyb
 			while (G_STATE.bet_input[i] != 0 && i < 32) {
 				i++;
 			}
-			
+
 			if (i > 0) {	// Do nothing if bet_input is empty
 				G_STATE.bet_input[i - 1] = 0;
 			}
 		}
 	}
-	
+
 	if (k_input->return_pressed) {
 		if (G_STATE.h_status == Showdown) {
 			G_STATE.flags &= ~HAND_INITIALIZED;
@@ -539,13 +543,13 @@ void update_and_render(win32_offscreen_buffer *buffer, game_assets *assets, keyb
 			if (G_STATE.players[0].stack >= bet_amount) {
 				G_STATE.pot += bet_amount;
 				G_STATE.players[0].stack -= bet_amount;
-				
+
 				clear_bet_input();
-				
+
 				// TODO: AI response. For now CPU just calls every time...
 				G_STATE.pot += bet_amount;
 				G_STATE.players[1].stack -= bet_amount;
-				
+
 				if (G_STATE.h_status == PreFlop) {
 					G_STATE.board[0] = extract_random_card(G_STATE.deck.cards);
 					G_STATE.board[1] = extract_random_card(G_STATE.deck.cards);
@@ -558,29 +562,29 @@ void update_and_render(win32_offscreen_buffer *buffer, game_assets *assets, keyb
 					// TODO: Find out actual 5 card hand to use for each player given the player's 2 card hand
 					// plus the board (all 5 card permutations of 7 cards) and solve issue with sorting (make a copy... figure
 					// out what the function should return).
-					
+
 					card combined_hand1[7];
 					combined_hand1[0] = G_STATE.players[0].hand[0];
 					combined_hand1[1] = G_STATE.players[0].hand[1];
-					
+
 					for (int i = 2; i < 7; i++) {
 						combined_hand1[i] = G_STATE.board[i - 2];
 					}
-					
+
 					card combined_hand2[7];
 					combined_hand2[0] = G_STATE.players[1].hand[0];
 					combined_hand2[1] = G_STATE.players[1].hand[1];
-					
+
 					for (int i = 2; i < 7; i++) {
 						combined_hand2[i] = G_STATE.board[i - 2];
 					}
-					
+
 					sort_combined_hand(combined_hand1);
 					sort_combined_hand(combined_hand2);
-					
+
 					best_possible_hand_result best_hand_result1 = get_best_possible_hand(combined_hand1);
 					best_possible_hand_result best_hand_result2 = get_best_possible_hand(combined_hand2);
-					
+
 					if (best_hand_result1.rank < best_hand_result2.rank) {
 						G_STATE.players[0].stack += G_STATE.pot;
 						G_STATE.winner_index = 0;
@@ -590,11 +594,11 @@ void update_and_render(win32_offscreen_buffer *buffer, game_assets *assets, keyb
 					} else {
 						G_STATE.winner_index = -1;	// Tie
 					}
-					
+
 					/*
 					G_STATE.ranks[0] = evaluate_hand(G_STATE.players[0].hand);
 					G_STATE.ranks[1] = evaluate_hand(G_STATE.players[1].hand);
-					
+
 					if (G_STATE.ranks[0] < G_STATE.ranks[1]) {
 						G_STATE.players[0].stack += G_STATE.pot;
 						G_STATE.winner_index = 0;
@@ -606,47 +610,47 @@ void update_and_render(win32_offscreen_buffer *buffer, game_assets *assets, keyb
 					}
 					*/
 				}
-				
+
 				increment_h_status();
 			}
 		}
 	}
-	
+
 	if (!(G_STATE.flags & DECK_CREATED)) {
 		create_deck(G_STATE.deck.cards);
 		G_STATE.flags |= DECK_CREATED;
 	}
-	
+
 	if (test_deck_empty(G_STATE.deck.cards)) {
 		create_deck(G_STATE.deck.cards);
 	}
-	
+
 	if (!(G_STATE.flags & GAME_INITIALIZED)) {
 		G_STATE.players[0].stack = 10000;
 		G_STATE.players[1].stack = 10000;
-		
+
 		G_STATE.flags |= GAME_INITIALIZED;
 	}
-	
+
 	if (!(G_STATE.flags & HAND_INITIALIZED)) {
 		srand(time(0));
-	
+
 		G_STATE.h_status = PreFlop;
 		G_STATE.pot = 150;
 		G_STATE.players[0].stack -= 50;
 		G_STATE.players[1].stack -= 100;	// CPU is just always big blind for now.
-	
+
 		hand_generator(G_STATE.players[0].hand, G_STATE.deck.cards);
 		hand_generator(G_STATE.players[1].hand, G_STATE.deck.cards);
-		
+
 		G_STATE.flags |= HAND_INITIALIZED;
 	}
-	
+
 	for (int i = 0; i < 2; i++) {
 		char id[3];
 		get_card_image_id(G_STATE.players[0].hand[i], id);
 		int asset_index = get_card_asset_index(id);
-		
+
 		render_bitmap(71 * i, 0, buffer, global_assets.card_images[asset_index].bmp);
 	}
 
@@ -658,9 +662,9 @@ void update_and_render(win32_offscreen_buffer *buffer, game_assets *assets, keyb
 			char id[3];
 			get_card_image_id(G_STATE.players[1].hand[i], id);
 			int asset_index = get_card_asset_index(id);
-			
+
 			render_bitmap(700 + (i * 71), 0, buffer, global_assets.card_images[asset_index].bmp);
-			
+
 			if (G_STATE.winner_index == 0) {
 				debug_render_string(600, 300, buffer, "Player");
 			} else if (G_STATE.winner_index == 1) {
@@ -671,42 +675,42 @@ void update_and_render(win32_offscreen_buffer *buffer, game_assets *assets, keyb
 
 		}
 	}
-	
+
 	if (G_STATE.h_status >= Flop) {
 		for (int i = 0; i < 3; i++) {
 			char id[3];
 			get_card_image_id(G_STATE.board[i], id);
 			int asset_index = get_card_asset_index(id);
-			
+
 			render_bitmap(200 + (71 * i), 200, buffer, global_assets.card_images[asset_index].bmp);
 		}
-		
+
 		if (G_STATE.h_status >= Turn) {
 		char id[3];
 			get_card_image_id(G_STATE.board[3], id);
 			int asset_index = get_card_asset_index(id);
-			
+
 			render_bitmap(200 + (71 * 3), 200, buffer, global_assets.card_images[asset_index].bmp);
-			
+
 			if (G_STATE.h_status >= River) {
 				get_card_image_id(G_STATE.board[4], id);
 				int asset_index = get_card_asset_index(id);
-				
+
 				render_bitmap(200 + (71 * 4), 200, buffer, global_assets.card_images[asset_index].bmp);
 			}
 		}
 	}
-	
-	
-	
+
+
+
 	char stack_str1[16];
 	itoa(G_STATE.players[0].stack, stack_str1, 10);
 	debug_render_string(200, 50, buffer, stack_str1);
-	
+
 	char stack_str2[16];
 	itoa(G_STATE.players[1].stack, stack_str2, 10);
 	debug_render_string(900, 50, buffer, stack_str2);
-	
+
 	char pot_str[16];
 	itoa(G_STATE.pot, pot_str, 10);
 	debug_render_string(500, 450, buffer, pot_str);
